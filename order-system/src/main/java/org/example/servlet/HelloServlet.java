1deabd36c0f64ab625612e6e44939a2c6a8b9d94
jojoss <beigecanon@gmail.com>
2025-06-04 18:35:37 -0700

    fix web.xml

---
+++ order-system/src/main/java/org/example/servlet/HelloServlet.java
@@ -0,0 +1,15 @@
+package org.example.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().write("Hello from Servlet");
+    }
+}
